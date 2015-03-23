6cbc5d17306baa84d51da48e04cd71ff325de212
Chris Dusto <cricket_lover@live.com>
2015-03-23 12:52:31 -0700

    v1.0 of dumper

---
+++ dump.pl
@@ -0,0 +1,226 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use XML::Simple qw(:strict);
+use FileHandle;
+use Data::Dumper;
+
+print STDERR "Loading wares.xml...";
+my $warexmlref = XMLin("xml/wares.xml",
+		ForceArray	=> [qw/ware effect production/],
+		KeyAttr		=> {effect => '+type'}
+	);
+
+print STDERR "\nLoading 0001-L044.xml...";
+my $langref = XMLin("xml/0001-L044.xml",
+		ForceArray	=> [qw /page t/],
+		KeyAttr		=> {page => '+id',
+				    t	 => '+id'}
+	);
+
+print STDERR "\nIndexing wares...";
+my %wares;
+foreach my $wareRef (@{$warexmlref->{ware}}) {
+	my %ware;
+	$ware{id} = $wareRef->{id};
+	$ware{name}=wareString($wareRef->{name});
+	($ware{specialist})=$wareRef->{specialist} =~ /specialist(.*)/ if defined $wareRef->{specialist};
+	$ware{specialist}='(none)' unless defined $ware{specialist};
+	$ware{volume}=$wareRef->{volume};
+	$ware{price}=$wareRef->{price}->{average};
+	$ware{transport}=$wareRef->{transport};
+	next if $ware{transport} eq 'ship';
+	next unless defined $wareRef->{tags};
+	next unless $wareRef->{tags} =~ /economy/;
+	my %productions;
+	foreach my $prodRef (@{$wareRef->{production}}) {
+		my %production;
+		$production{time}=$prodRef->{time};
+		my %outputs;
+		$outputs{$ware{id}} = $prodRef->{amount};
+		$production{what}=\%outputs;
+		my %inputs;
+		foreach my $input (@{$prodRef->{primary}->{ware}}) {
+			$inputs{$input->{ware}}=$input->{amount};
+		}
+		$production{inputs}=\%inputs;
+		my %secondary;
+		foreach my $second (@{$prodRef->{secondary}->{ware}}) {
+			$secondary{$second->{ware}}=$second->{amount};
+		}
+		$production{secondary}=\%secondary;
+		$productions{$prodRef->{method}}=\%production;
+	}
+	$ware{productions}=\%productions;
+	$wares{$ware{id}}=\%ware;
+}
+
+print STDERR "\nCollecting production modules...";
+my @prodModuleList = split /\n/,`ls xml/struct_econ_prod_*_macro.xml`;
+my %prodModules;
+foreach my $xmlName (@prodModuleList) {
+	my $ref = XMLin($xmlName,
+		ForceArray	=> [qw /item/],
+		KeyAttr		=> {item	=> '+ware'}
+		);
+	my %module;
+	$module{id}=$ref->{macro}->{name};
+	next unless defined $ref->{macro}->{properties}->{identification}->{name};
+	$module{name}=string($ref->{macro}->{properties}->{identification}->{name});
+	next unless defined $ref->{macro}->{properties}->{production}->{wares};
+	my @waresMade = split / /,$ref->{macro}->{properties}->{production}->{wares};
+	my %wareMethods;
+	foreach my $ware (@waresMade) {
+		if(defined $ref->{macro}->{properties}->{production}->{queue}->{item}->{$ware}->{method}) {
+			$wareMethods{$ware}=$ref->{macro}->{properties}->{production}->{queue}->{item}->{$ware}->{method};
+		} else {
+			$wareMethods{$ware}='default';
+		}
+	}
+	$module{makes}=\%wareMethods;
+	$prodModules{$module{id}}=\%module;
+}
+
+print STDERR "\nCollecting stations...";
+my @stationXmlList = split /\n/,`ls xml/struct_bt_*_macro.xml`;
+my %stations;
+foreach my $xmlName (@stationXmlList) {
+	my $ref = XMLin($xmlName,
+		ForceArray	=> [qw /connection/],
+		KeyAttr		=> {}
+		);
+	my $stationRef = $ref->{macro};
+	my %station;
+	$station{id}=$stationRef->{name};
+	$station{name}=string($stationRef->{properties}->{identification}->{name});
+	my @prodModules;
+	foreach my $connection (@{$stationRef->{connections}->{connection}}) {
+		next unless defined $connection->{macro};
+		next unless defined $connection->{macro}->{ref};
+		next unless ($connection->{macro}->{ref} =~ /struct_econ_prod_.*_macro/);
+		my %module;
+		$module{macro}=$connection->{macro}->{ref};
+		if(defined $connection->{build}) {
+			$module{build}=$connection->{build}->{sequence} . '-' . $connection->{build}->{stage};
+		} else {
+			$module{build} = "N/A";
+		}
+		push @prodModules, \%module;
+	}
+	$station{prodModules} = \@prodModules;
+	$stations{$station{id}}=\%station;
+}
+
+print STDERR "\n";
+
+my ($stationID,$stationNameNice,$prodLevel,$prodModuleName,$prodTimeNice,$multiNeed,$multiOptional,$multiOutput,$multiIntermediate);
+
+format LISTALL =
+@*
+$stationID
+@* Ware Summary (Fully built)
+$stationNameNice
+@|||||||||||||||||||||||| || @|||||||||||||||||||||||| || @|||||||||||||||||||||||| || @||||||||||||||||||||||||
+"NEED",				    "OPTIONAL",				"INTERMEDIATE",			    "OUTPUT"
+^|||||||||||||||||||||||| || ^|||||||||||||||||||||||| || ^|||||||||||||||||||||||| || ^|||||||||||||||||||||||| ~~
+$multiNeed,			    $multiOptional,			$multiIntermediate,		    $multiOutput
+
+
+
+.
+
+format_name STDOUT "LISTALL";
+foreach $stationID (sort keys %stations) {
+	$stationNameNice = $stations{$stationID}->{name};
+	my %usedWares; # Need Optional Intermediate Produce
+	foreach my $prodModule (@{$stations{$stationID}->{prodModules}}) {
+		my $refProd = $prodModules{$prodModule->{macro}};
+		next unless defined $refProd;
+		foreach my $prodWare (keys %{$refProd->{makes}}) {
+			my $refWareMaker = $wares{$prodWare}->{productions}->{$refProd->{makes}->{$prodWare}};
+			markAll('P',$refWareMaker->{what},\%usedWares);
+			markAll('N',$refWareMaker->{inputs},\%usedWares);
+			markAll('O',$refWareMaker->{secondary},\%usedWares);
+		}
+	}
+	next unless scalar keys %usedWares;
+	my (@need,@optional,@intermediate,@output);
+	foreach my $ware (sort keys %usedWares) {
+		push @need,$wares{$ware}->{name} if $usedWares{$ware} eq 'N';
+		push @optional,$wares{$ware}->{name} if $usedWares{$ware} eq 'O';
+		push @intermediate,$wares{$ware}->{name} if $usedWares{$ware} eq 'I';
+		push @output,$wares{$ware}->{name} if $usedWares{$ware} eq 'P';
+	}
+	$multiNeed = join "\r",sort @need;
+	$multiOptional = join "\r",sort @optional;
+	$multiIntermediate = join "\r",sort @intermediate;
+	$multiOutput = join "\r",sort @output;
+	$multiNeed = '(none)' if $multiNeed eq '';
+	$multiOptional = '(none)' if $multiOptional eq '';
+	$multiIntermediate = '(none)' if $multiIntermediate eq '';
+	$multiOutput = '(none)' if $multiOutput eq '';
+	write STDOUT;
+}
+
+##########
+
+sub wareString {
+	my $ref = string(shift);
+	if($ref =~ /{.*} {.*}/) {
+		my ($name,$mark) = split / /,$ref;
+		$ref = string($name) . ' ' . string($mark);
+	}
+	return $ref;
+}
+
+sub string {
+	my $ref = shift;
+	my ($page,$id) = $ref =~ /{([0-9]*),([0-9]*)}/;
+	return $ref unless defined $id;
+	return $langref->{page}->{$page}->{t}->{$id}->{content};
+}
+
+sub multiWare {
+	my $ref = shift;
+	my $result = '';
+	foreach my $ware (sort keys %{$ref}) {
+		$result .= $ref->{$ware}." x ".$wares{$ware}->{name} . "\r";
+	}
+	chomp $result;
+	if($result eq '') {
+		$result = '(none)';
+	}
+	return $result
+}
+
+sub markWith {
+	my ($val,$ware,$ref) = @_;
+	my %mapping = (	N => {	N => 'N',
+				O => 'N',
+				I => 'I',
+				P => 'I'},
+			O => {	N => 'N',
+				O => 'O',
+				I => 'I',
+				P => 'I'},
+			I => {	N => 'I',
+				O => 'I',
+				I => 'I',
+				P => 'I'},
+			P => {	N => 'I',
+				O => 'I',
+				I => 'I',
+				P => 'P'});
+	if(defined $ref->{$ware}) {
+		$ref->{$ware}=$mapping{$ref->{$ware}}->{$val};
+	} else {
+		$ref->{$ware}=$val;
+	}
+}
+
+sub markAll {
+	my ($val,$wares,$ref) = @_;
+	foreach my $ware (keys %{$wares}) {
+		markWith($val,$ware,$ref);
+	}
+}
